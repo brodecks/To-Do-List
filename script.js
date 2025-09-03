9d0659285865c3651f66b19c24c9373da5985663
Brodecks <raphae.donikian@oulootk.fr>
2025-09-03 20:30:29 +0200

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-document.getElementById('btnAdd').addEventListener('click', function(){
-    document.querySelector('.list').innerHTML = `${document.getElementById('txtTask').value}`;
-    document.getElementById('txtTask').value = '';
-})
+document.getElementById('btnAdd').addEventListener('click', function() {
+    const taskValue = document.getElementById('txtTask').value;
+    if (taskValue.trim() !== '') {
+        const li = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        li.textContent = taskValue;
+        document.querySelector('.list').appendChild(li);
+        li.appendChild(checkbox);
+        document.getElementById('txtTask').focus();
+        document.getElementById('txtTask').value = '';
+    }
+});
