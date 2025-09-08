399d5a767420940542bcb4bad7ceca6f9b9c0841
Brodecks <raphae.donikian@oulootk.fr>
2025-09-08 22:48:57 +0200

    third commit : strugling to add a message when all of the tasks are completed

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 document.getElementById('btnAdd').addEventListener('click', function() {
     const taskValue = document.getElementById('txtTask').value;
     if (taskValue.trim() !== '') {
-        const li = document.createElement('li');
+        const p = document.createElement('p');
         const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
-        li.textContent = taskValue;
-        document.querySelector('.list').appendChild(li);
-        li.appendChild(checkbox);
+        p.textContent = taskValue;
+        document.querySelector('.list').appendChild(p);
+        p.appendChild(checkbox);
         document.getElementById('txtTask').focus();
         document.getElementById('txtTask').value = '';
+    }
+});
+
+document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
+    if(checkbox.checked){
+        const pa = document.createElement('p');
+        pa.textContent = 'Bravo ! Vous avez realise toutes vos taches.';
+        document.querySelector('.list').appendChild(pa);
     }
 });
